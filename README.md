ae81293202c74aa9cdb445087d1f8d09c9f0edfb
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2025-07-10 20:31:58 +0200

    Added crypto module

--- README.md
+++ README.md
@@ -7,15 +7,61 @@ This is a library you can use to interact with kolme-powered applications. Thing
 ```TypeScript
 import { KolmeClient } from 'kolme-client'
 
-const client = new KolmeClient('https://yourkolme.app')
+const client = new KolmeClient('https://yourkolme.app');
 
-const privateKey = client.generatePrivateKey()
+const privateKey = client.generatePrivateKey();
 
 const block = await client.broadcast(privateKey, [{
   YourAppMessage: {
     YourAppPayload: {}
   }
-}])
+}]);
+```
+
+## Store the private key
+
+We do not assume whether or how you want to store the private key, but in case you do want to store it, you probably also want to encrypt it. For that we provide an `encrypt` and `decrypt` functions that can get you started. They use the standard browser's crypto API and the `PBKDF2` algorithm. What we do not provide is an encryption key. You will have to figure that one out yourself.
+
+Here's an example of storing the private key as a hex string in localstorage:
+
+```TypeScript
+import { encrypt, decrypt } from 'kolme-client/crypto'
+import { KolmeClient } from 'kolme-client'
+
+const client = new KolmeClient('https://yourkolme.app');
+
+const generateAndStorePrivateKey = async (encryptionKey: string) => {
+  const privateKey = client.generatePrivateKey();
+  const encryptedPrivateKey = await encrypt({
+    message: Buffer.from(privateKey).toString('hex'), 
+    key: encryptionKey 
+  });
+
+  localStorage.setItem('your_key', encryptedPrivateKey)
+}
+
+const getPrivateKey = async (encryptionKey: string) => {
+  const encryptedPrivateKey = localStorage.getItem('your_key');
+
+  const privateKey = await decrypt({
+    encryptedMessage: encryptedPrivateKey, 
+    key: encryptionKey
+  });
+
+  return Buffer.from(privateKey, 'hex')
+}
+
+// You need to figure out how to get and secure this
+const encryptionKey = '123456789';
+
+await generateAndStorePrivateKey(encryptionKey);
+const privateKey = await getPrivateKey(encryptionKey);
+
+const block = await client.broadcast(privateKey, [{
+  YourAppMessage: {
+    YourAppPayload: {}
+  }
+}]);
 ```
 
 ## Listen to notifications on the kolme's websocket endpoint
@@ -23,16 +69,16 @@ const block = await client.broadcast(privateKey, [{
 ```TypeScript
 import { KolmeClient } from 'kolme-client'
 
-const client = new KolmeClient('https://yourkolme.app')
+const client = new KolmeClient('https://yourkolme.app');
 
-const client.subscribeToNotifications(
+client.subscribeToNotifications(
   (message) => {  
     // Do something interesting with the message
   },
   (socketState) => {  
     // Get updates about the socket state - may be useful for React re-rendering for example
   }
-)
+);
 
 // Stop listening after a minute
 setTimeout(
@@ -40,5 +86,5 @@ setTimeout(
     client.unsubscribeFromNotifications()
   },
   60 * 1000
-)
+);
 ```
--- src/crypto.ts
+++ src/crypto.ts
@@ -1,4 +1,7 @@
 const DIGEST_ALGO = 'SHA-256';
+const SALT_LENGTH = 16;
+const INITIALIZATION_VECTOR_LENGTH = 12;
+const ITERATIONS = 1000000
 
 export const digest = async (message: string): Promise<Uint8Array> => {
   const msgBuffer = new TextEncoder().encode(message);
@@ -6,3 +9,118 @@ export const digest = async (message: string): Promise<Uint8Array> => {
   return new Uint8Array(hashBuffer);
 };
 
+export const encrypt = async ({
+  message,
+  key,
+}: {
+  message: string;
+  key: string;
+}): Promise<string> => {
+  const encoder = new TextEncoder();
+  const messageUtf8 = encoder.encode(message);
+  const keyUtf8 = encoder.encode(key);
+
+  const salt = crypto.getRandomValues(new Uint8Array(SALT_LENGTH));
+  const iv = crypto.getRandomValues(new Uint8Array(INITIALIZATION_VECTOR_LENGTH));
+
+  const cryptoKey = await crypto.subtle.importKey(
+    'raw',
+    keyUtf8,
+    'PBKDF2',
+    false,
+    ['deriveKey']
+  );
+
+  const aesKey = await crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations: ITERATIONS,
+      hash: 'SHA-256'
+    },
+    cryptoKey,
+    {
+      name: 'AES-GCM',
+      length: 256
+    },
+    false,
+    ['encrypt']
+  );
+
+  const encryptedMessage = await crypto.subtle.encrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+      tagLength: 128
+    },
+    aesKey,
+    messageUtf8
+  );
+
+  const encrypted = new Uint8Array(salt.length + iv.length + encryptedMessage.byteLength)
+
+  encrypted.set(salt)
+  encrypted.set(iv, salt.length)
+  encrypted.set(new Uint8Array(encryptedMessage), salt.length + iv.length)
+
+  return btoa(String.fromCharCode(...encrypted))
+};
+
+export const decrypt = async ({
+  encryptedMessage,
+  key,
+}: {
+  encryptedMessage: string;
+  key: string;
+}): Promise<string> => {
+  const encoder = new TextEncoder();
+  const keyUtf8 = encoder.encode(key);
+
+  const binaryString = atob(encryptedMessage);
+
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  
+  const salt = bytes.slice(0, SALT_LENGTH);
+  const iv = bytes.slice(SALT_LENGTH, SALT_LENGTH + INITIALIZATION_VECTOR_LENGTH);
+  const encryptedData = bytes.slice(SALT_LENGTH + INITIALIZATION_VECTOR_LENGTH);
+
+  const cryptoKey = await crypto.subtle.importKey(
+    'raw',
+    keyUtf8,
+    'PBKDF2',
+    false,
+    ['deriveKey']
+  );
+
+  const aesKey = await crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations: ITERATIONS,
+      hash: 'SHA-256'
+    },
+    cryptoKey,
+    {
+      name: 'AES-GCM',
+      length: 256
+    },
+    false,
+    ['decrypt']
+  );
+
+  const message = await crypto.subtle.decrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+      tagLength: 128
+    },
+    aesKey,
+    encryptedData
+  );  
+
+  const decoder = new TextDecoder();
+  return decoder.decode(message);
+};
